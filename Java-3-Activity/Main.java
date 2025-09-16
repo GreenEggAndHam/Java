e30d679e8a76aedaf082b9b1a30573310e2aca93
GreenEggAndHam <adrianc406@nycstudents.net>
2025-09-16 16:21:57 +0000

    comment

---
+++ Java-3-Activity/Main.java
@@ -0,0 +1,18 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+
+  // Init function which runs when the 
+  // program begins.  
+
+  void init(){
+   System.out.println("Hi");
+   System.out.println("My name is Adrian Xavier Caraballo-Camacho");
+   System.out.println("My name is Adrian Xavier Caraballo-Camacho and I live on Elm Street.");
+   System.out.println("My name is Adrian Xavier Caraballo-Camacho \nI happen to live on the Elm Street.");
+    
+  }
+
+  
+}
---
+++ Java-3b-CW/Main.java
@@ -0,0 +1,57 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+  // This is a single line comment in Java
+  // Multi-line comments are enclosed within  /*  and  */
+  
+  /* 
+  Init function which runs when 
+  the program begins is invoked(called) from within the Main function.
+  */
+  void init(){
+    // print Welcome
+    public class Main {
+      public static void main(String [] args){
+        system.out.println("Welcome");
+      }
+    }
+
+    // print Hello with the quotes displayed
+        public class Main {
+      public static void main(String [] args){
+        system.out.println(" \" Hello");
+      }
+    }
+    
+    // print \Java Code\ as shown
+      public class Main {
+      public static void main(String [] args){
+        system.out.println(" \\Java Code\\");
+      }
+    }
+    
+    // print the name Joe tabbed once
+    public class Main {
+      public static void main(String [] args){
+        system.out.println("\+ Joe ");
+      }
+    }
+
+    // print the name Mary tabbed twice
+    public class Main {
+      public static void main(String [] args){
+        system.out.println("\+ Mary ");
+      }
+    }
+    
+    // print one, two and three on separate lines using a single line of code
+      public class Main {
+      public static void main(String [] args){
+        system.out.println(" one \n two \n three ");
+      }
+    }
+	
+  }
+  
+}
